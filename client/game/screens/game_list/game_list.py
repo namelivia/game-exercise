0642fd462183df3e705df35a084a6a2864d85b0e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-04-23 17:08:27 +0200

    Add TODO

--- client/game/screens/game_list/game_list.py
+++ client/game/screens/game_list/game_list.py
@@ -3,6 +3,7 @@ from .ui import GameListTitle, Background
 from client.events import UserTypedEvent
 
 
+# TODO: This is currently blocked because the server only answers with Game Messages
 class GameList(Screen):
     def __init__(self, client_state):
         super().__init__(client_state)
