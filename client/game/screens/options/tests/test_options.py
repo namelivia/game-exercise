bcfe2984e8f6947f06724725c8b4a65fbc5be46b
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-08-19 17:15:25 +0200

    Add visual test for options

---
+++ client/game/screens/options/tests/test_options.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from client.game.screens.options.options import Options
+from client.visual_regression.visual_regression import VisualRegression
+import mock
+
+
+class TestOptions(TestCase):
+    def setUp(self):
+        self.client_state = mock.Mock()
+        self.client_state.clock.get.return_value = 0  # Initial time is 0
+        self.options = Options(self.client_state)
+
+    def test_visual_regression(self):
+        VisualRegression.assert_matches_snapshot(
+            self.options,
+            "./client/game/screens/options/tests/screenshots/options.png"
+        )
