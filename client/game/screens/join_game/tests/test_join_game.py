35ac595854d5c531d71bba159f9733e2296ba93b
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-08-19 16:51:03 +0200

    Add more tests

---
+++ client/game/screens/join_game/tests/test_join_game.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from client.game.screens.join_game.join_game import JoinGame
+from client.events import UserTypedEvent
+from client.visual_regression.visual_regression import VisualRegression
+import mock
+
+
+class TestJoinGame(TestCase):
+    def setUp(self):
+        self.client_state = mock.Mock()
+        self.client_state.clock.get.return_value = 0  # Initial time is 0
+        self.join_game = JoinGame(self.client_state)
+
+    @mock.patch("client.commands.RequestJoiningAGame")
+    def test_join_game_screen(self, m_request_join_game):
+
+        # Empty screen
+        VisualRegression.assert_matches_snapshot(
+            self.join_game,
+            "./client/game/screens/join_game/tests/screenshots/join_game_no_input.png"
+        )
+
+        # User types the word test
+        self.join_game.update(
+            UserTypedEvent("t"),
+        )
+        self.join_game.update(
+            UserTypedEvent("e"),
+        )
+        self.join_game.update(
+            UserTypedEvent("s"),
+        )
+        self.join_game.update(
+            UserTypedEvent("t"),
+        )
+
+        # Typo
+        self.join_game.update(
+            UserTypedEvent("z"),
+        )
+        # Deleting last letter
+        self.join_game.update(
+            UserTypedEvent("backspace"),
+        )
+
+        VisualRegression.assert_matches_snapshot(
+            self.join_game,
+            "./client/game/screens/join_game/tests/screenshots/join_game_test_input.png")
+
+        # User presses enter and creates the new game
+        self.join_game.update(
+            UserTypedEvent("return"),
+        )
+        m_request_join_game.assert_called_once_with(mock.ANY, mock.ANY, "test")
