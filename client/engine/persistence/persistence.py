3823fa177e28a736859948ace85b49a440617375
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-01-24 19:32:15 +0100

    Type persistence

--- client/engine/persistence/persistence.py
+++ client/engine/persistence/persistence.py
@@ -1,19 +1,22 @@
 import pickle
+from typing import TYPE_CHECKING, Iterable
 from os import walk
-from .persistence_dto import PersistenceDTO
+
+if TYPE_CHECKING:
+    from .persistence_dto import PersistenceDTO
 
 PATH = "client_data/"
 
 
 class Persistence:
     @staticmethod
-    def load(key) -> PersistenceDTO:
+    def load(key: str) -> "PersistenceDTO":
         return pickle.load(open(f"{PATH}{key}", "rb"))
 
     @staticmethod
-    def save(data: PersistenceDTO, key):
+    def save(data: "PersistenceDTO", key: str) -> None:
         pickle.dump(data, open(f"{PATH}{key}", "wb"))
 
     @staticmethod
-    def list():
+    def list() -> Iterable:
         return next(walk(PATH))[2]
