2974691f38b20560aacc24faea7c59e8c6d086c0
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-01-24 23:06:45 +0100

    More typing

--- client/engine/server_polling.py
+++ client/engine/server_polling.py
@@ -1,22 +1,26 @@
 from client.engine.features.synchronization.commands import RequestGameStatus
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from client.engine.client_state import ClientState
 
 
 class ServerPolling:
     @staticmethod
-    def _get_polling_rate():
+    def _get_polling_rate() -> int:
         # The polling rate is how often the server is queried for new information
         return 100
 
     @staticmethod
-    def _is_polling_time(time):
+    def _is_polling_time(time: int) -> bool:
         return time % ServerPolling._get_polling_rate() == 0
 
     @staticmethod
-    def _is_playing_a_game(game_id):
+    def _is_playing_a_game(game_id: str) -> bool:
         return game_id is not None
 
     @staticmethod
-    def _should_do_polling(client_state):
+    def _should_do_polling(client_state: "ClientState"):
         game_id = client_state.profile.game_id
         time = client_state.clock.get()
         return ServerPolling._is_playing_a_game(
@@ -24,7 +28,7 @@ class ServerPolling:
         ) and ServerPolling._is_polling_time(time)
 
     @staticmethod
-    def push_polling_event_if_needed(client_state):
+    def push_polling_event_if_needed(client_state: "ClientState"):
         if ServerPolling._should_do_polling(client_state):
             RequestGameStatus(
                 client_state.profile,
