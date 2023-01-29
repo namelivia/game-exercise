ee58ae7c52550d7416b4faeb0acd1717d9d36f30
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-01-29 13:19:37 +0100

    More mypy

--- client/engine/events_processor.py
+++ client/engine/events_processor.py
@@ -1,16 +1,22 @@
+from typing import TYPE_CHECKING, List
 import logging
 
 logger = logging.getLogger(__name__)
 
+if TYPE_CHECKING:
+    from client.engine.primitives.event_handler import EventHandler
+    from client.engine.general_state.client_state import ClientState
+    from client.engine.primitives.event import Event
+
 
 class EventsProcessor:
-    def __init__(self, event_handlers):
+    def __init__(self, event_handlers: List["EventHandler"]):
         self.event_handlers = event_handlers  # Initial list of event handlers
 
-    def add_event_handler(self, event_handler):
+    def add_event_handler(self, event_handler: "EventHandler") -> None:
         self.event_handlers.append(event_handler)
 
-    def handle(self, event, client_state):
+    def handle(self, event: "Event", client_state: "ClientState") -> None:
         if event is None:
             return
         handled = False
--- client/engine/server_polling.py
+++ client/engine/server_polling.py
@@ -20,7 +20,7 @@ class ServerPolling:
         return game_id is not None
 
     @staticmethod
-    def _should_do_polling(client_state: "ClientState"):
+    def _should_do_polling(client_state: "ClientState") -> bool:
         game_id = client_state.profile.game_id
         time = client_state.clock.get()
         return ServerPolling._is_playing_a_game(
@@ -28,7 +28,7 @@ class ServerPolling:
         ) and ServerPolling._is_polling_time(time)
 
     @staticmethod
-    def push_polling_event_if_needed(client_state: "ClientState"):
+    def push_polling_event_if_needed(client_state: "ClientState") -> None:
         if ServerPolling._should_do_polling(client_state):
             RequestGameStatus(
                 client_state.profile,
