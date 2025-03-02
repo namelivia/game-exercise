c2f42ba3208ef3f40af7d002cbb7b0a871ac5b6c
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-03-03 00:46:37 +0100

    Remove comment

--- client/engine/graphics/graphics.py
+++ client/engine/graphics/graphics.py
@@ -12,7 +12,7 @@ class Graphics:
 
     def render(self, screen: "Screen") -> None:
         ui_elements = screen.get_ui_elements()
-        self.window.fill((255, 255, 255))  # Clear the window (only pygame)
+        self.window.fill((255, 255, 255))
         for ui_element in ui_elements:
             ui_element.render(self.window)
         FoundationalWrapper.update_display()
