90e99dff808eaae1f5bc7773fdc206ea485e2887
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-03-03 14:03:52 +0100

    Fix some typing issues

--- client/engine/external/foundational_wrapper.py
+++ client/engine/external/foundational_wrapper.py
@@ -1,3 +1,5 @@
+from typing import Any, List, Tuple
+
 import pygame
 
 
@@ -21,63 +23,64 @@ class FoundationalWrapper:
     MOUSEBUTTONDOWN = pygame.MOUSEBUTTONDOWN
 
     @staticmethod
-    def get_event():
+    def get_event() -> List[pygame.event.Event]:
         return pygame.event.get()
 
     @staticmethod
-    def quit():
+    def quit() -> None:
         pygame.quit()
         return None
 
     @staticmethod
-    def update_display():
+    def update_display() -> None:
         pygame.display.update()
         return None
 
     @staticmethod
-    def get_new_window(width: int, height: int):
+    def get_new_window(width: int, height: int) -> pygame.Surface:
         return pygame.display.set_mode((width, height))
 
     @staticmethod
-    def play_sound(path: str):
+    def play_sound(path: str) -> None:
         pygame.mixer.Sound.play(pygame.mixer.Sound(path))
         return None
 
     @staticmethod
-    def load_music():
+    def load_music(path: str) -> None:
         pygame.mixer.music.load(path)
         return None
 
     @staticmethod
-    def play_music():
+    def play_music() -> None:
         pygame.mixer.music.play(-1)
         return None
 
     @staticmethod
-    def stop_music():
+    def stop_music() -> None:
         pygame.mixer.music.stop()
         return None
 
     @staticmethod
-    def get_clock_ticks():
+    def get_clock_ticks() -> int:
         return int(pygame.time.get_ticks())
 
     @staticmethod
-    def get_mouse_position():
+    def get_mouse_position() -> Tuple[int, int]:
         return pygame.mouse.get_pos()
 
     @staticmethod
-    def load_image(path: str):
+    def load_image(path: str) -> pygame.Surface:
         return pygame.image.load(path)
 
+    # TODO: I had to add Any here as I could not find a way to type hint this
     @staticmethod
-    def sprite_group():
+    def sprite_group() -> Any:
         return pygame.sprite.Group()
 
     @staticmethod
-    def get_default_font():
+    def get_default_font() -> str:
         return pygame.font.get_default_font()
 
     @staticmethod
-    def get_font(font, size):
+    def get_font(font: str, size: int) -> pygame.font.Font:
         return pygame.font.Font(font, size)
--- client/engine/primitives/shape.py
+++ client/engine/primitives/shape.py
@@ -14,11 +14,24 @@ class Shape(ABC):
     def show(self) -> None:
         self.hidden = False
 
+    def get_x(self) -> int:
+        return self.x
+
+    def get_y(self) -> int:
+        return self.y
+
     def set_x(self, x: int) -> None:
         self.x = x
 
     def set_y(self, y: int) -> None:
         self.y = y
+
+    # TODO: Transform these two into @abstractmethod
+    def get_width(self) -> int:
+        return 0
+
+    def get_height(self) -> int:
+        return 0
 
     # TODO: Can I type pygame types?
     def render(self, window: Any) -> None:
