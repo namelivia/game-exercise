08255af08d1039f2b4f132557ff87940390eae90
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-03-07 00:50:30 +0100

    Remove legacy function

--- client/engine/general_state/clock.py
+++ client/engine/general_state/clock.py
@@ -2,8 +2,5 @@ from client.engine.external.foundational_wrapper import FoundationalWrapper
 
 
 class Clock:
-    def tick(self) -> None:
-        pass
-
     def get(self) -> int:
         return FoundationalWrapper.get_clock_ticks()
