2b6a43c1ef12ba56007f017ec36876e7dc2b2eac
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-10-02 23:20:29 +0200

    Create a script to exclusively test sound

---
+++ run_sound_check.py
@@ -0,0 +1,19 @@
+import pygame
+
+from client.engine.features.sound.commands import PlaySound
+from client.engine.features.sound.worker import SoundWorker
+from client.engine.general_state.options import Options
+from client.engine.general_state.queue import QueueManager
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.mixer.init()
+    QueueManager().initialize()
+    Options().initialize()
+    sound_thread = SoundWorker(
+        name="Sound",
+        queue=QueueManager().get("sound"),
+    )
+
+    sound_thread.start()
+    PlaySound("client/experiment/sounds/elephant.mp3").execute()
