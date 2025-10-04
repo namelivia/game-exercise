2039e863e8cf6ec2052f53c23894065652a12a5d
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-10-04 17:26:34 +0200

    Undo

--- client/experiment/sounds.py
+++ client/experiment/sounds.py
@@ -1,7 +1,7 @@
 # SOUNDS
-LION = "client/experiment/assets/sounds/lion.mp3"
-GIRAFFE = "client/experiment/assets/sounds/giraffe.mp3"
-ELEPHANT = "client/experiment/assets/sounds/elephant.mp3"
-RHINO = "client/experiment/assets/sounds/rhino.mp3"
-CHEETAH = "client/experiment/assets/sounds/cheetah.mp3"
-HYENA = "client/experiment/assets/sounds/hyena.mp3"
+LION = "client/experiment/sounds/lion.mp3"
+GIRAFFE = "client/experiment/sounds/giraffe.mp3"
+ELEPHANT = "client/experiment/sounds/elephant.mp3"
+RHINO = "client/experiment/sounds/rhino.mp3"
+CHEETAH = "client/experiment/sounds/cheetah.mp3"
+HYENA = "client/experiment/sounds/hyena.mp3"
