29b39ab5e786b4c54f814aad5909cada3d5f39e9
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-10-23 17:10:31 +0200

    Add an Animation to the render feature test

--- client/feature_tests/render.py
+++ client/feature_tests/render.py
@@ -2,7 +2,7 @@ from client.engine.clock import Clock
 from client.engine.features.render.commands import StartRendering
 from client.engine.features.render.worker import RenderWorker
 from client.engine.general_state.queue import QueueManager
-from client.engine.graphics.shapes import Image, Rectangle, Text
+from client.engine.graphics.shapes import Animation, Image, Rectangle, Text
 from client.engine.primitives.screen import Screen
 from client.engine.primitives.ui import create_ui_element
 
@@ -15,6 +15,7 @@ class TestScreen(Screen):
                     Image("client/experiment/images/background.png", 0, 0),
                     Text(f"This is a test", 20, 20),
                     Rectangle(0, 0, 20, 20),
+                    Animation("client/game/images/coin", 250, 0, 3),
                 ]
             )
         ]
