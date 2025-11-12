99172da361dd8f9f8851a45685cb4a859cef07db
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-11-12 22:15:48 +0100

    Reorganize file

--- client/engine/engine/api.py
+++ client/engine/engine/api.py
@@ -7,7 +7,7 @@ from engine.features.game_logic.commands import ChangeCursor, ScreenTransition
 # SOUND Commands
 from engine.features.sound.commands import PlayMusic, PlaySound
 
-# USER events
+# INPUT events
 from engine.features.user_input.events import UserClickedEvent, UserTypedEvent
 
 # Shapes
@@ -18,10 +18,10 @@ from engine.primitives.event import Event
 from engine.primitives.event_handler import EventHandler
 from engine.primitives.screen import Screen
 from engine.primitives.timer import Timer
-from engine.ui.animation.factory import create_animation
-from engine.ui.clickable import ClickableUIElement
 
 # UI
+from engine.ui.animation.factory import create_animation
+from engine.ui.clickable import ClickableUIElement
 from engine.ui.factory import create_ui_element
 from engine.ui.logic import UIElementLogic
 from engine.ui.state import UIElementState
