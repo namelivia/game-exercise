97e5abdefbdffbc6b7e58f767f1cd158d5d4c0d7
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-11-20 00:15:48 +0100

    Port the easy parts

--- client/demos/game/game/screens/enter_name/enter_name.py
+++ client/demos/game/game/screens/enter_name/enter_name.py
@@ -1,8 +1,6 @@
-from client.engine.features.sound.commands import PlaySound
-from client.engine.features.user_input.events import UserTypedEvent
-from client.engine.primitives.screen import Screen
+from engine.api import PlaySound, Screen, ScreenTransition, UserTypedEvent
 
-from .ui import Background, EnterNameMessage
+from .ui import create_background
 
 
 class EnterName(Screen):
@@ -12,35 +10,30 @@ class EnterName(Screen):
         self.data = {"name": ""}
 
         self.ui_elements = [
-            Background(),
-            EnterNameMessage(self.data["name"]),
+            create_background(),
+            # EnterNameMessage(self.data["name"]),
         ]
 
         self.events = {UserTypedEvent: self.on_user_typed}
 
     def on_user_typed(self, event: UserTypedEvent) -> None:
         if event.key == "escape":
-            # Avoid circular import
-            from client.game.commands import BackToLobby
+            from game.screens.lobby.lobby import Lobby
 
-            BackToLobby().execute()
-            return
+            ScreenTransition(Lobby).execute()
         if event.key == "return":
-            # Avoid circular import
-            from client.engine.commands import SetPlayerName
-            from client.game.commands import BackToLobby
+            # SetPlayerName(self.data["name"]).execute()
+            from game.screens.lobby.lobby import Lobby
 
-            SetPlayerName(self.data["name"]).execute()
-            BackToLobby().execute()
-            pass
+            ScreenTransition(Lobby).execute()
         if event.key == "backspace":
             PlaySound(
-                "client/game/sounds/erase.mp3",
+                "assets/sounds/erase.mp3",
             ).execute()
             self.data["name"] = self.data["name"][:-1]
             return
         else:
             PlaySound(
-                "client/game/sounds/type.mp3",
+                "assets/sounds/type.mp3",
             ).execute()
             self.data["name"] += event.key
