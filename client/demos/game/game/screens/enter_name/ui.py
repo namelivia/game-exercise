0265f29fc6e97490333aa2a4e5cdd68debb36d98
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-11-25 14:21:25 +0100

    WIP

--- client/demos/game/game/screens/enter_name/ui.py
+++ client/demos/game/game/screens/enter_name/ui.py
@@ -6,9 +6,9 @@ from engine.api import UIBuilder, UIElementLogic
 class NameInputCustomLogic(UIElementLogic):
     def update(self, time: int, data: Dict[str, Any]) -> None:
         name = data["name"]
-        # name_text = self.shapes[2]
-        # if isinstance(name_text, Text):
-        # name_text.set_message(name)  # Not supersure about this
+        name_text = self.find_shape("name")
+        if name_text is not None:
+            name_text.set_message(name)
 
 
 def create_name_input(name: str):
@@ -16,7 +16,7 @@ def create_name_input(name: str):
         UIBuilder(x=20, y=0)
         .with_text("Set name", 0, 0)
         .with_text("Please enter your player name", 0, 40)
-        .with_text(name, 0, 70)
+        .with_text(name, 0, 70, True, "name")
         .with_logic(NameInputCustomLogic)
         .build()
     )
