b8307f1aa894bd55bb8a3b2fd2047fbb1f017cf4
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-12-05 16:35:57 +0100

    Reduce scope more

--- client/demos/labyrinth/run.py
+++ client/demos/labyrinth/run.py
@@ -1,7 +1,6 @@
 import logging
 
 from engine.api import start_application
-from labyrinth.screen_loader import load_screen
 from labyrinth.screens.main.main import MainScreen
 
 if __name__ == "__main__":
@@ -11,7 +10,4 @@ if __name__ == "__main__":
         format="[%(asctime)s] %(message)s",
     )
 
-    screen = load_screen("labyrinth/screens/main/scene.json")
-    print(screen.__dict__)
-    exit()
     start_application(initial_screen=MainScreen)
